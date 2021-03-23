9ca54d65addeb70e66042bcd361432224f228928
Aydrian Howard <aydrian@gmail.com>
2021-03-23 15:54:26 -0400

    added form submission trigger function

---
+++ functions/submission-created.js
@@ -0,0 +1,19 @@
+const { CourierClient } = require("@trycourier/courier");
+const courier = CourierClient();
+
+exports.handler = async function (event, context) {
+  // your server-side functionality
+  const { payload: data, site } = JSON.parse(event.body);
+  console.log("site: ", site);
+  console.log("payload: ", data);
+
+  const event = process.env.COURIER_EVENT || "NETLIFY_FORM_SUBMISSION";
+  const recipientId = process.env.COURIER_RECIPIENT;
+  const list = process.env.COURIER_LIST;
+
+  if (list) {
+    await courier.lists.send({ event, list, data });
+  } else if (recipientId) {
+    await courier.send({ eventId: event, recipientId, data });
+  }
+};
