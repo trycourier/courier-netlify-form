bdae581a41ffcb9e0bbb361e8e0c3f8581ab0444
Aydrian Howard <aydrian@gmail.com>
2021-03-24 08:48:10 -0400

    submission-created function clean up

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,18 +1,45 @@
 const { CourierClient } = require("@trycourier/courier");
 const courier = CourierClient();
 
-exports.handler = async function (event, context) {
-  // your server-side functionality
-  const { payload: data, site } = JSON.parse(event.body);
-  console.log("payload: ", data);
+exports.handler = async function (event) {
+  const {
+    payload: { data, form_name }
+  } = JSON.parse(event.body);
 
-  const eventId = process.env.COURIER_EVENT || "NETLIFY_FORM_SUBMISSION";
+  const eventId =
+    process.env.COURIER_EVENT || `NETLIFY_FORM_${form_name.toUpperCase()}`;
   const recipientId = process.env.COURIER_RECIPIENT;
   const list = process.env.COURIER_LIST;
 
   if (list) {
-    await courier.lists.send({ event: eventId, list, data });
+    try {
+      const { messageId } = await courier.lists.send({
+        event: eventId,
+        list,
+        data
+      });
+      console.log(`Message sent to Courier List ${list}: ${messageId}`);
+    } catch (error) {
+      console.log(
+        `An error occurred sending to the Courier List ${list}:`,
+        error
+      );
+    }
   } else if (recipientId) {
-    await courier.send({ eventId, recipientId, data });
+    try {
+      const { messageId } = await courier.send({ eventId, recipientId, data });
+      console.log(
+        `Message sent to Courier Recipient ${recipientId}: ${messageId}`
+      );
+    } catch (error) {
+      console.log(
+        `An error occured sending to the Courier Recipient ${recipientId}:`,
+        error
+      );
+    }
+  } else {
+    console.log(
+      `No message sent. Please set a list or recipient using the environment variables.`
+    );
   }
 };
